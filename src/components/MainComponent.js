80ba8f4a91eeb7a7dae0df1aae39b84d042adad6
basiljiji <basiljiji2015@gmail.com>
2022-09-19 23:16:35 +0530

    Downgraded React-Router-Dom to 5.2.0

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-import { Routes, Route, useParams } from 'react-router-dom';
-import { TOPPERS } from "../shared/toppers";
-import { PLACEMENTS } from "../shared/placements";
-import { ANNOUNCEMENTS } from "../shared/announcement";
-import { POSTS } from "../shared/posts";
+import { Switch, Route, useParams, withRouter, Redirect } from 'react-router-dom';
 import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import Footer from "./FooterComponent";
@@ -12,57 +8,57 @@ import Announcement from "./AnnouncementComponent";
 import AnnouncementDetail from "./AnnouncementDetailComponent";
 import Post from "./PostComponent";
 import PostDetail from "./PostDetailComponent";
+import { connect } from "react-redux";
 
+const mapStateToProps = state =>{
+  return{
+    toppers : state.toppers,
+    placements: state.placements,
+    announcements : state.announcements,
+    posts: state.posts
 
-
-class Main extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      toppers: TOPPERS,
-      placements: PLACEMENTS,
-      announcements: ANNOUNCEMENTS,
-      posts: POSTS
-    }
   }
+}
+
+class Main extends Component{
+
+    constructor(props){
+      super(props);
+    }
   render(){
     
     const Homepage = () =>{
       return(
         <Home 
-        topper={this.state.toppers} 
-        placement={this.state.placements} />
+        topper={this.props.toppers} 
+        placement={this.props.placements} />
       )
     }
 
-    const AnnouncementWithId = () =>{
-      const params = useParams();
+    const AnnouncementWithId = ({match}) =>{
       return(
-        <AnnouncementDetail announcement={this.state.announcements.filter((announcement) => announcement.id === parseInt((params.announcementId), 10))[0]}
+        <AnnouncementDetail announcement={this.props.announcements.filter((announcement) => announcement.id === parseInt((match.params.announcementId), 10))[0]}
         />
       )
     }
 
-    const PostWithId = () =>{
-      const params = useParams();
+    const PostWithId = ({match}) =>{
       return(
-        <PostDetail post = {this.state.posts.filter((post) => post.id === parseInt((params.postId), 10))[0]} />
+        <PostDetail post = {this.props.posts.filter((post) => post.id === parseInt((match.params.postId), 10))[0]} />
       )
     }
 
     return (
       <div className="App">
           <Header />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/home" element={<Homepage />} />
-            <Route path="/contactus" element={<Contact />} />
-            <Route path="/announcements" element={ <Announcement announcement={this.state.announcements}/>} />
-            <Route path="/announcements/:announcementId" element={<AnnouncementWithId />} />
-            <Route path="/posts" element={<Post post={this.state.posts} />} />
-            <Route path="/posts/:postId" element={<PostWithId />} />
-            <Route path="*" element={<Homepage />}/> {/* Change it into and error page by creating a new error */}
-          </Routes>
+          <Switch>
+            <Route path="/home" component={Homepage} />
+            <Route exact path="/announcements" component={() => <Announcement announcement={this.props.announcements}/>} />
+            <Route path="/announcements/:announcementId" component={AnnouncementWithId} />
+            <Route exact path="/posts" component={() => <Post post={this.props.posts} />} />
+            <Route path="/posts/:postId" component={PostWithId} />
+            <Route exact path="/contactus" component={Contact} />
+          </Switch>
           
           <Footer />
       </div>
@@ -70,4 +66,4 @@ class Main extends Component {
 }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
